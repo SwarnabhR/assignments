52d3ccdf16fb8bf66aa20da701651c85210eff7c
Swarnabh R. <workspace.swarnabh@gmail.com>
2025-06-22 20:41:21 +0530

    Commit new assignment questions

--- week-2/week-2-js/medium/countVowels.js
+++ week-2/week-2-js/medium/countVowels.js
@@ -6,7 +6,15 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  const vowels = "aeiouAEIOU";
+
+  let count = 0;
+  for (let i of str) {
+    if (vowels.includes(i)) {
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = countVowels;
